c4d1cd0a61d5e84d726e65a3b8c5f775b34b0947
531devv <531devv@gmail.com>
2022-06-09 16:05:08 +0200

    Adding task files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,99 @@
-const mapData = (data: any): any => {
-  return data
-}
+import {
+  Data,
+  SingleData,
+  PropsData,
+  PropsSingleData,
+  Gender,
+  Size,
+  Direction,
+} from "./types";
+
+let email: string | null;
+let phone: number | null;
+
+const isEmail = (email: string): boolean => email.includes("@");
+
+const convertGender = (gender: string): Gender => {
+  if (gender === "M") {
+    return "Male";
+  } else {
+    return "Female";
+  }
+};
+
+const handleComponentSize = (component: HTMLElement) => {
+  const sizes: Size[] = ["small", "medium", "large"];
+  const [currentSize] = sizes.filter((size) => {
+    if (component.classList.contains(size)) return size;
+  });
+  return currentSize;
+};
+
+const onButtonClick = (direction: Direction, component: HTMLElement): void => {
+  const currentSize = handleComponentSize(component);
+
+  if (direction === "grow") {
+    switch (currentSize) {
+      case "small":
+        component.classList.remove(currentSize);
+        component.classList.add("medium");
+        break;
+      case "medium":
+        component.classList.remove(currentSize);
+        component.classList.add("large");
+        break;
+    }
+  }
+
+  if (direction === "shrink") {
+    switch (currentSize) {
+      case "large":
+        component.classList.remove(currentSize);
+        component.classList.add("medium");
+        break;
+      case "medium":
+        component.classList.remove(currentSize);
+        component.classList.add("small");
+    }
+  }
+};
+
+const convertSize = (size: string): Size => {
+  switch (size) {
+    case "S":
+      return "small";
+    case "M":
+      return "medium";
+    case "L":
+      return "large";
+    default:
+      return "medium";
+  }
+};
+
+const mapData = (data: PropsData): Data => {
+  const sanitanizedData = data.map((item: PropsSingleData): SingleData => {
+    const fullname: string = `${item.first_name}  ${item.last_name}`;
+    email = null;
+    phone = null;
+
+    isEmail(item.contact)
+      ? (email = item.contact)
+      : (phone = parseInt(item.contact));
+
+    return {
+      name: fullname,
+      pictureSource: item.image,
+      occupation: item.occupation,
+      gender: convertGender(item.gender),
+      about: item.about,
+      size: convertSize(item.card_size),
+      onButtonClick: onButtonClick,
+      email: email,
+      phone: phone,
+    };
+  });
+  return sanitanizedData;
+};
 
 export default mapData;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,32 @@
-export enum GENDER {
-  MALE,
-  FEMALE,
-  APACHE_HELICOPTER,
+export type Gender = "Male" | "Female";
+export type Size = "small" | "medium" | "large";
+export type Email = string | null;
+export type PhoneNumber = number | null;
+export type Text = string;
+export type Direction = "shrink" | "grow";
+
+export interface PropsSingleData {
+  first_name: string;
+  last_name: string;
+  gender: string;
+  image: URL;
+  occupation: string;
+  about: string;
+  card_size: Size;
+  contact: string;
 }
 
 export interface SingleData {
-  firstName: string;
-  lastName: string;
-  age: number;
-  gender: GENDER;
-  occupation?: string;
+  name: string;
+  pictureSource: URL;
+  occupation: string;
+  gender: Gender;
+  about: Text;
+  size: Size;
+  onButtonClick(direction: Direction, component): void;
+  email?: Email;
+  phone?: PhoneNumber;
 }
 
+export type PropsData = PropsSingleData[];
 export type Data = SingleData[];
